d4225c15966ace831ad95c7cc2421658e3bdb7f8
Sushan Khatiwada <sushan.khatiwada2064@gmail.com>
2025-07-04 03:54:09 +0000

    Restored to '230890c13f1d9b22981b60077d3fa54d08af63d1'
    Replit-Restored-To: 230890c13f1d9b22981b60077d3fa54d08af63d1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,16 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "./context/ThemeContext";
 import Home from "./pages/Home";
 
 function App() {
 
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
