b2c1b0847b783b52ddb13ecacb11e3228adb059f
Sushan Khatiwada <sushan.khatiwada2064@gmail.com>
2025-10-02 08:06:02 +0545

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4ju2n@$f9d0c=h)_g0lbb%k9&@rf(xa$d$g$&5ri$uf)*gev^4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["https://sushan-ko-pasal.onrender.com", "*"]
 #ALLOWED_HOSTS = os.environ["REPLIT_DOMAINS"].split(',') + ['localhost', '127.0.0.1']
